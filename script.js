7e5678dbbd09bf9b2f37887325d6a7e2fcd735b3
Calvin Yorn <calvin_yorn@student.uml.edu>
2025-09-25 16:11:52 -0400

    cursor

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const text = "Welcome to My Cybersecurity Blog";  //header effect
-const typingElement = document.getElementById("type_effect"); // grabs id from html
+const typingElement = document.getElementById("type_text"); // now targets the inner span where characters are injected
 
 let index = 0; // current character index
 
-// Typing effect function
+// typing effect function
 function typeEffect() {
     if (index < text.length) {
-        typingElement.textContent += text.charAt(index);
+    typingElement.textContent += text.charAt(index);
         index++;
-        setTimeout(typeEffect, 100); // typing speed (ms)
+        setTimeout(typeEffect, 400); // typing speed (ms)
+    } else {
+        // once done, wait a bit, then clear and restart
+        setTimeout(() => {
+            typingElement.textContent = "";
+            index = 0;
+            typeEffect();
+        }, 4000); // pause before restarting (4 seconds here)
     }
 }
-
-// Start typing when page loads
+// start typing when page loads
 window.onload = typeEffect;
 
-
-//
+ 
